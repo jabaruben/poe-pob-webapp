24a674d5555c8099c4c654e18186766ee9f2c77b
Álex de la Puente <alejandro.delapuente@nexplore.com>
2021-04-22 01:29:40 +0200

    Init poe-pob-api

---
+++ poe-pob-api/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.post('/hola', function (req, res) {
+  res.send('[POST]Saludos desde express');
+});
+app.get('/hola', function (req, res) {
+  res.send('[GET]Saludos desde express');
+});
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
