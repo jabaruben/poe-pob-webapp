b92d2dde8d9d2c1630310eedc9e669f9d7392167
Álex de la Puente <alejandro.delapuente@nexplore.com>
2021-04-29 00:49:22 +0200

    components directory

--- poe-pob-webapp/src/app/app.module.ts
+++ poe-pob-webapp/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './../material/material.module';
 
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
