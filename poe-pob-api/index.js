ac09daa16be38d2822fb4f0408685df3499ed0ee
Álex de la Puente <alejandro.delapuente@nexplore.com>
2021-04-29 02:17:53 +0200

    Add API headers

--- poe-pob-api/index.js
+++ poe-pob-api/index.js
@@ -11,6 +11,16 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 app.get('/', (req, res) => {
   res.send('API works!');
 });
@@ -45,7 +55,10 @@ function verifyUser(req, res, next) {
   if (validateUser)
     next();
   else
-    res.sendStatus(403);
+    res.json({
+      "success": 0,
+      "message": "login failed"
+    });
 }
 
 // FORMAT OF TOKEN
