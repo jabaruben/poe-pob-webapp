936f1d69d5946c41179c461837d04ddfb22ef0e8
Álex de la Puente <alejandro.delapuente@nexplore.com>
2021-05-29 17:03:55 +0200

    fix loading issue

--- poe-pob-webapp/src/app/modules/init/init.module.ts
+++ poe-pob-webapp/src/app/modules/init/init.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { InitRoutingModule } from './init-routing.module';
 import { MaterialModule } from 'src/material/material.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { InitComponent } from './components/init/init.component';
 
@@ -15,10 +13,7 @@ import { InitComponent } from './components/init/init.component';
   imports: [
     CommonModule,
     InitRoutingModule,
-    MaterialModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    MaterialModule    
   ]
 })
 export class InitModule { }
